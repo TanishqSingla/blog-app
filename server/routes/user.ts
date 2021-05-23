4ca220de739a92e0cf2426613c664b0c3deac875
TanishqSingla <tanishqsingla00@gmail.com>
2021-05-23 23:09:37 +0530

    added user routes

---
+++ server/routes/user.ts
@@ -0,0 +1,15 @@
+const userController = require("../controllers/user.ctrl");
+
+module.exports = (router) => {
+  // Get a user
+  router.route("/user/:id").get(userController.getUser);
+
+  // get a user profile
+  router.route("/user/profile/:id").get(userController.getUserProfile);
+
+  // adds a user
+  router.route.post(userController.addUser);
+
+  // follow a user
+  router.route("/user/follow").post(userController.followUser);
+};
