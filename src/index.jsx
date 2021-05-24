37392eec0811e08a4c2072f2bee453f5643c7513
TanishqSingla <tanishqsingla00@gmail.com>
2021-05-24 19:38:26 +0530

    Adding router

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Switch, Route} from 'react-router-dom'; 
+import {ConnectedRouter} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {store, history} from './redux/store'
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
     <App />
-  </React.StrictMode>,
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
